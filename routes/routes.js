740e6400975841df0fa0a43605d0b23b844b8618
Reeka <reekamaharaj@gmail.com>
2020-05-26 22:54:18 -0500

    routes file working now

--- routes/routes.js
+++ routes/routes.js
@@ -1,21 +1,44 @@
-const db = require("./models");
+const db = require("../models");
 const axios = require("axios");
 const cheerio = require("cheerio");
 
-module.exports = function(app) {
+module.exports = function(app){
     app.get("/", function(req, res) {
         res.render("index");
     });
-    
+
     app.get("/saved", function(req, res) {
         res.render("saved");
     });
-    
+
     app.get("/login", function(req, res) {
         res.send("login");
     });
-    
+
     app.get("/register", function(req, res) {
         res.send("register");
     });
+
+    app.get("/scrape", function(req, res) {
+        axios.get("https://www.goodnewsnetwork.org/category/news/animals/").then(function(res) {
+        let $ = cheerio.load(res.data);
+            $("div .td-block-span6").each(function(i, element) {
+                let result = {};
+                result.title = $(this).children("a").attr("title");
+                result.link = $(this).children("a").attr("href");
+                result.img = $(this).children("img").attr("src");
+
+                db.Article.create(result)
+                .then(function (dbArticle){
+                    console.log(dbArticle);
+                })
+                .catch(function (err) {
+                    console.log(err);
+                });
+            })
+    });
+    res.send("scrape");
+    });
 }
+
+//boredpanda, lovemeow, the onion, 
--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 //Dependancies
 const express = require("express");
 const mongoose = require("mongoose");
-const logger = require("morgan");
 const exphbs = require("express-handlebars");
 
 //Express setup
@@ -9,7 +8,6 @@ const PORT = 3000;
 const app = express();
 
 //Middleware
-app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
@@ -17,31 +15,11 @@ app.use(express.static("public"));
 //Handlebars
 app.engine("handlebars", exphbs({ defaultLayout: "main"}));
 app.set("view engine", "handlebars");
-// require("./routes/routes")(app);
+require("./routes/routes")(app);
 
 //Connect to Mongo DB
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
-mongoose.connect(MONGODB_URI);
-
-app.get("/", function(req, res) {
-    res.render("index");
-});
-
-app.get("/saved", function(req, res) {
-    res.render("saved");
-});
-
-app.get("/login", function(req, res) {
-    res.send("login");
-});
-
-app.get("/register", function(req, res) {
-    res.send("register");
-});
-
-app.get("/scrape", function(req, res) {
-    res.send("scrape");
-});
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 // Start the server
 app.listen(PORT, function () {
